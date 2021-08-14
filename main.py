9f78cca923e581f673927ee7710fc7b806345b85
Mimoriam <mimoriam420@gmail.com>
2021-08-14 21:38:32 +0500

    Commit #2 - Path parameters (localhost:8000/items/abc) with Enums

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
+
+
+class Model(str, Enum):
+    abc = "abc"
+    xyz = "xyz"
 
 
 @app.get("/")
 async def index():
     return {"Hello": "World"}
+
+
+@app.get("/items/{model}")
+async def items(model: Model):
+    return {"model": model}
