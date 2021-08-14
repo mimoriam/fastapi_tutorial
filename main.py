0cc0f7b2fdbe7460d4a254f9d652fb9322e475d4
Mimoriam <mimoriam420@gmail.com>
2021-08-14 21:55:02 +0500

    Commit #3 - Query parameters (after the ?)

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from enum import Enum
+from typing import Optional
 
 app = FastAPI()
-
-
-class Model(str, Enum):
-    abc = "abc"
-    xyz = "xyz"
 
 
 @app.get("/")
@@ -14,6 +9,13 @@ async def index():
     return {"Hello": "World"}
 
 
-@app.get("/items/{model}")
-async def items(model: Model):
-    return {"model": model}
+@app.get("/items/")
+async def items(wow: int = 0, limit: int = 10):
+    # http://localhost:8000/items/?wow=4&limit=20
+    return {wow: limit}  # This line would give {"4" : 20} response based on that query parameter
+
+
+@app.get("/items/a")
+async def items(wow: Optional[str] = None):
+    # http://localhost:8000/items/a?wow
+    return {wow}  # This would give nothing because wow is now optional
