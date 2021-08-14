9561db5524866e15b3cf109ad5c517a0e4fd5414
Mimoriam <mimoriam420@gmail.com>
2021-08-15 00:59:32 +0500

    Commit #6.5 - Body validation was missing, added it~

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Path, Query, Body
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -12,12 +12,12 @@ async def index():
 
 class Item(BaseModel):
     name: str
-    description: Optional[str] = None
+    description: Optional[str] = Field(None, max_length=255)
 
 
 @app.post("/items/{id}/")
 # http://localhost:8000/items/2/?query=Hello
-# Note that Path(), Body() & Query() have the same optional parameters
+# Note that Path(), Body()/Field() & Query() have the same optional parameters
 async def create_item(
         id: int = Path(..., lt=3, gt=0, title="Id of item"),
         item: Item = Body(...),
@@ -27,5 +27,5 @@ async def create_item(
         "item": item
     }
 
-# https://fastapi.tiangolo.com/tutorial/body-multiple-params/
-# More on that in this link ^^
+# https://fastapi.tiangolo.com/tutorial/body-fields/
+# Body validation using Fields() ^^
